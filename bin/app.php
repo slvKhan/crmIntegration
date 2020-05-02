47f522efe42680ca39a593e98b42bc848444306b
slvKhan <hanviacheslaw@gmail.com>
2020-05-02 20:27:12 +0300

    feat: authorization

--- bin/app.php
+++ bin/app.php
@@ -5,14 +5,20 @@ use App\Client;
 require_once __DIR__.'/../vendor/autoload.php';
 
 $user = ['slayerhan1997', 'slayer.han1997@gmail.com', '0ff60cca81234b0532ed48e5f7597c6d00eb1628'];
+$cookiePath = 'bin/cookieJar.txt';
+$headers =  [
+    'User-Agent' => 'amoCRM-API-client/1.0',
+    'content-type' => 'application/json',
+];
 
-$app = new Client(new Guzzle());
+$cookieJar =  new GuzzleHttp\Cookie\FileCookieJar($cookiePath, TRUE);
+$app = new Client(new GuzzleHttp\Client(['cookies' => $cookieJar, 'headers' => $headers]));
 
 try {
     $app->authorization(...$user);
     echo 'Авторизация прошла успешно';
-} catch($e) {
-    'Авторизация провалилась!';
-    printf($e);
+} catch (Exception $e) {
+    echo 'Авторизация провалилась!';
 }
+
 
--- src/Client.php
+++ src/Client.php
@@ -4,36 +4,29 @@ namespace App;
 
 class Client
 {
-    private $subdomain;
-    private $login;
-    private $hash;
     private $client;
 
     public function __construct($httpClient)
     {
-        $client = $httpClient;
+        $this->client = $httpClient;
     }
 
     public function authorization($subdomain, $login, $hash)
     {
-        $this->subdomain = $subdomain;
-        $this->login = $login;
-        $this->hash = $hash;
         $link = 'https://' . $subdomain . '.amocrm.ru/private/api/auth.php?type=json';
         $user = [
             'USER_LOGIN' => $login,
             'USER_HASH' => $hash,
         ];
-        $headers =  [
-            'User-Agent' => 'amoCRM-API-client/1.0',
-            'content-type' => 'application/json',
-        ];
-
-        $response = $this->client->request('POST', $link, $headers, json_encode($user));
+        $response = $this->client->post($link, [
+            'body' => json_encode($user),
+        ]);
         $code = $response->getStatusCode();
+
         if ($code != 200 && $code != 204) {
             throw new Exception($code);
         }
+        print_r(json_decode($response->getBody())); 
         return true;
     }
 
