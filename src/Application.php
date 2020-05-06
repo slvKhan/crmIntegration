bb9d21b1ab5ff9d5d2f436046492146f1ea1ca81
slvKhan <hanviacheslaw@gmail.com>
2020-05-06 17:19:10 +0300

    ref

--- src/Application.php
+++ src/Application.php
@@ -26,32 +26,35 @@ class Application
             echo $succes ? $this->messages['authSucces'] : $this->messages['authFaild'];
             $leads = $this->service->listOf('leads');
             echo $this->messages['fetchLeads'];
-        
-            $emptyLeadsID = $leads
-                ->filter(function ($lead) {
-                    return $lead['closest_task_at'] === 0;
-                })
-                ->map(function ($lead) {
-                    return [
-                        'element_id' => $lead['id'],
-                        'responsible_user_id' => $lead['responsible_user_id']
-                    ];
-                })
-                ->all();
-        
-            if (count($emptyLeadsID) === 0) {
-                echo $this->messages['empty'];
-                die;
-            }
-        
-            $tasksList = array_map(function ($lead) {
-                return models\Task::taskForEmptyDeal($lead);
-            }, $emptyLeadsID);
-        
+            $tasksList = $this->getTasksOfEmptyLeads($leads);
             $this->service->addTasks($tasksList);
             echo $this->messages['addTaskSucces'];
         } catch (Exception $e) {
             echo $this->messages['connectFail'] . $e;
         }
+    }
+
+    private function getTasksOfEmptyLeads(models\Lead $leads): array
+    {
+        $emptyLeadsID = $leads
+        ->filter(function ($lead) {
+            return $lead['closest_task_at'] === 0;
+        })
+        ->map(function ($lead) {
+            return [
+                'element_id' => $lead['id'],
+                'responsible_user_id' => $lead['responsible_user_id']
+            ];
+        })
+        ->all();
+
+        if (count($emptyLeadsID) === 0) {
+            echo $this->messages['empty'];
+            die;
+        }
+
+        return array_map(function ($lead) {
+            return models\Task::taskForEmptyDeal($lead);
+        }, $emptyLeadsID);
     }
 }
