d678cb17039438e7fca9a58368465c8fa667e0b9
slvKhan <hanviacheslaw@gmail.com>
2020-05-06 16:55:42 +0300

    ref

--- bin/app.php
+++ bin/app.php
@@ -5,14 +5,6 @@ namespace App;
 require_once __DIR__.'/../vendor/autoload.php';
 
 $cookiePath = 'bin/cookieJar.txt';
-$messages = [
-    'authSucces' => "Вы авторизованы \n",
-    'authFaild' => "Логин или пароль не верны! \n",
-    'connectFail' => 'Ошибка подключения код: ',
-    'fetchLeads' => "Сделки загружены \n",
-    'addTaskSucces' => "Задачи добавленны \n",
-    'empty' => "У всех сделок есть задачи \n"
-];
 $httpParams = [
     'cookies' => new \GuzzleHttp\Cookie\FileCookieJar($cookiePath, true),
     'headers' =>  [
@@ -22,37 +14,6 @@ $httpParams = [
 ];
 
 $httpClient = new \GuzzleHttp\Client($httpParams);
-$app = new Service($httpClient);
-
-try {
-    $succes = $app->authorization(new models\User());
-    echo $succes ? $messages['authSucces'] : $messages['authFaild'];
-    $leads = $app->listOf('leads');
-    echo $messages['fetchLeads'];
-
-    $emptyLeadsID = $leads
-        ->filter(function ($lead) {
-            return $lead['closest_task_at'] === 0;
-        })
-        ->map(function ($lead) {
-            return [
-                'element_id' => $lead['id'],
-                'responsible_user_id' => $lead['responsible_user_id']
-            ];
-        })
-        ->all();
-
-    if (count($emptyLeadsID) === 0) {
-        echo $messages['empty'];
-        die;
-    }
-
-    $tasksList = array_map(function ($lead) {
-        return models\Task::taskForEmptyDeal($lead);
-    }, $emptyLeadsID);
-
-    $app->addTasks($tasksList);
-    echo $messages['addTaskSucces'];
-} catch (Exception $e) {
-    echo $messages['connectFail'] . $e;
-}
+$service = new Service($httpClient);
+$app = new Application($service);
+$app->run();
---
+++ src/Application.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+class Application
+{
+    private $service;
+    private $messages = [
+        'authSucces' => "Вы авторизованы \n",
+        'authFaild' => "Логин или пароль не верны! \n",
+        'connectFail' => 'Ошибка подключения код: ',
+        'fetchLeads' => "Сделки загружены \n",
+        'addTaskSucces' => "Задачи добавленны \n",
+        'empty' => "У всех сделок есть задачи \n"
+    ];
+
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
+
+    public function run(): void
+    {
+        try {
+            $succes = $this->service->authorization(new models\User());
+            echo $succes ? $this->messages['authSucces'] : $this->messages['authFaild'];
+            $leads = $this->service->listOf('leads');
+            echo $this->messages['fetchLeads'];
+        
+            $emptyLeadsID = $leads
+                ->filter(function ($lead) {
+                    return $lead['closest_task_at'] === 0;
+                })
+                ->map(function ($lead) {
+                    return [
+                        'element_id' => $lead['id'],
+                        'responsible_user_id' => $lead['responsible_user_id']
+                    ];
+                })
+                ->all();
+        
+            if (count($emptyLeadsID) === 0) {
+                echo $this->messages['empty'];
+                die;
+            }
+        
+            $tasksList = array_map(function ($lead) {
+                return models\Task::taskForEmptyDeal($lead);
+            }, $emptyLeadsID);
+        
+            $this->service->addTasks($tasksList);
+            echo $this->messages['addTaskSucces'];
+        } catch (Exception $e) {
+            echo $this->messages['connectFail'] . $e;
+        }
+    }
+}
